c40cd7852dbc0d18fb8454e700a7a31378c3f6c5
fred <domaine75@gmail.com>
2021-10-04 01:36:42 +0200

    change editor config

--- test/AssemblyTester.cs
+++ test/AssemblyTester.cs
@@ -1,23 +1,26 @@
-﻿using System.Collections.Generic;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using SchemaZen.Library.Models;
+using System.Collections.Generic;
 
-namespace SchemaZen.Tests {
-	[TestFixture]
-	public class AssemblyTester {
-		[Test]
-		[TestCase("SAFE_ACCESS", "SAFE")]
-		[TestCase("UNSAFE_ACCESS", "UNSAFE")]
-		[TestCase("EXTERNAL_ACCESS", "EXTERNAL_ACCESS")]
-		public void Assembly_WithPermissionSetCases(string permissionSet,
-			string scriptedPermissionSet) {
-			var assembly = new SqlAssembly(permissionSet, "SchemazenAssembly");
-			assembly.Files.Add(new KeyValuePair<string, byte[]>("mydll", new byte[0]));
+namespace SchemaZen.Tests
+{
+		[TestFixture]
+		public class AssemblyTester
+		{
+				[Test]
+				[TestCase("SAFE_ACCESS", "SAFE")]
+				[TestCase("UNSAFE_ACCESS", "UNSAFE")]
+				[TestCase("EXTERNAL_ACCESS", "EXTERNAL_ACCESS")]
+				public void Assembly_WithPermissionSetCases(string permissionSet,
+					string scriptedPermissionSet)
+				{
+						var assembly = new SqlAssembly(permissionSet, "SchemazenAssembly");
+						assembly.Files.Add(new KeyValuePair<string, byte[]>("mydll", new byte[0]));
 
-			var expected = @"CREATE ASSEMBLY [SchemazenAssembly]
+						var expected = @"CREATE ASSEMBLY [SchemazenAssembly]
 FROM 0x
 WITH PERMISSION_SET = " + scriptedPermissionSet;
-			Assert.AreEqual(expected, assembly.ScriptCreate());
+						Assert.AreEqual(expected, assembly.ScriptCreate());
+				}
 		}
-	}
 }
